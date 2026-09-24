2758e6ea844f8828fb9f85474be0e0983c429e99
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2026-09-24 10:22:15 -0400

    feat(android): add model capability types

---
+++ android/edge-frameworks-core/src/main/kotlin/io/github/rajibsarwar/edgeframeworks/EdgeCapability.kt
@@ -0,0 +1,11 @@
+package io.github.rajibsarwar.edgeframeworks
+
+enum class EdgeCapability {
+    TEXT_GENERATION,
+    STREAMING,
+    STRUCTURED_OUTPUT,
+    TOOL_CALLING,
+    VISION,
+    AUDIO,
+    EMBEDDINGS
+}

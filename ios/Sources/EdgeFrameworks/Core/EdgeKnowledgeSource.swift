ef4caa915f7c879dc54a046b808af96e9805042c
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2026-09-24 23:33:11 -0400

    feat(ios): add knowledge source model

---
+++ ios/Sources/EdgeFrameworks/Core/EdgeKnowledgeSource.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+public struct EdgeKnowledgeSource: Codable, Equatable, Sendable {
+    public let id: String
+    public let collectionID: String
+    public let sourceIdentifier: String
+    public let contentFingerprint: String
+    public let documentIDs: [String]
+    public let metadata: [String: String]
+    public let indexedAtMilliseconds: Int64
+
+    public init(
+        id: String,
+        collectionID: String,
+        sourceIdentifier: String,
+        contentFingerprint: String,
+        documentIDs: [String],
+        metadata: [String: String] = [:],
+        indexedAtMilliseconds: Int64 = Int64(
+            Date().timeIntervalSince1970 * 1_000
+        )
+    ) {
+        self.id = id
+        self.collectionID = collectionID
+        self.sourceIdentifier = sourceIdentifier
+        self.contentFingerprint = contentFingerprint
+        self.documentIDs = documentIDs
+        self.metadata = metadata
+        self.indexedAtMilliseconds = indexedAtMilliseconds
+    }
+}
+
+public enum EdgeSourceSyncResult: Equatable, Sendable {
+    case unchanged(EdgeKnowledgeSource)
+    case indexed(
+        source: EdgeKnowledgeSource,
+        removedDocumentCount: Int,
+        indexedDocumentCount: Int
+    )
+}
